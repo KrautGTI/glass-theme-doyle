e270dd3bc34c454349a5ae67ff92f33dc5946f12
KrautGTI <tylerjamesdoyle@gmail.com>
2024-10-03 17:15:36 -0600

    Delete build directory

--- build/Release/.deps/Release/displayconfig.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/displayconfig.node := c++ -bundle -undefined dynamic_lookup -Wl,-search_paths_first -mmacosx-version-min=10.15 -arch x86_64 -L./Release -stdlib=libc++  -o Release/displayconfig.node  
--- build/Release/.deps/Release/vibrancy.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/vibrancy.node := c++ -bundle -undefined dynamic_lookup -Wl,-search_paths_first -mmacosx-version-min=10.15 -arch x86_64 -L./Release -stdlib=libc++  -o Release/vibrancy.node  
